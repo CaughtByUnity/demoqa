b5ae77b71579015448254eeaea73e0c9ddae1beb
caughtbyunity <xammer3500@gmail.com>
2025-09-21 17:51:33 +0300

    homework #11: раскоментировал 1 из тестов.

--- tests/tests_hw/test_webtables_hw.py
+++ tests/tests_hw/test_webtables_hw.py
@@ -2,40 +2,40 @@ from pages.tables import Tables
 import time
 from selenium.webdriver.common.keys import Keys
 
-# def test_tables_rows(browser):
-#     page_tables = Tables(browser)
-#     page_tables.visit()
-#     # Check that submitting empty form does not work by clicking "submit" after clicking "add"
-#     page_tables.btn_add.click()
-#     page_tables.btn_reg_form_submit.click()
-#     assert page_tables.btn_reg_form_submit.visible()
-#     page_tables.btn_reg_form_close.click()
-#     time.sleep(2)
-#     # filling the form and checking that reg form closes and we have a new row in table
-#     page_tables.btn_add.click()
-#     page_tables.inp_reg_form_first_name.send_keys('tester')
-#     page_tables.inp_reg_form_last_name.send_keys('testerovich')
-#     page_tables.inp_reg_form_user_email.send_keys('tester@ttt.tt')
-#     page_tables.inp_reg_form_age.send_keys('27')
-#     page_tables.inp_reg_form_salary.send_keys('5000')
-#     page_tables.inp_reg_form_department.send_keys('QA')
-#     page_tables.btn_reg_form_submit.click()
-#     time.sleep(2)
-#     assert not page_tables.btn_reg_form_submit.exist()
-#     assert page_tables.new_row_first_name.get_text() == 'tester'
-#     # checking 'Edit' button
-#     time.sleep(2)
-#     page_tables.btn_edit.click()
-#     # checking that 'Edit' shows filled reg form
-#     assert page_tables.inp_reg_form_first_name.get_dom_attribute('value') == 'Cierra'
-#     # change first name and check if row in table update
-#     page_tables.inp_reg_form_first_name.clear()
-#     page_tables.inp_reg_form_first_name.send_keys('Francesca')
-#     page_tables.btn_reg_form_submit.click()
-#     assert page_tables.old_row_first_name.get_text() == 'Francesca'
-#     # check 'Delete' button (delete row)
-#     page_tables.btn_delete_row.click()
-#     assert page_tables.old_row_first_name.get_text() == 'Alden'
+def test_tables_rows(browser):
+    page_tables = Tables(browser)
+    page_tables.visit()
+    # Check that submitting empty form does not work by clicking "submit" after clicking "add"
+    page_tables.btn_add.click()
+    page_tables.btn_reg_form_submit.click()
+    assert page_tables.btn_reg_form_submit.visible()
+    page_tables.btn_reg_form_close.click()
+    time.sleep(2)
+    # filling the form and checking that reg form closes and we have a new row in table
+    page_tables.btn_add.click()
+    page_tables.inp_reg_form_first_name.send_keys('tester')
+    page_tables.inp_reg_form_last_name.send_keys('testerovich')
+    page_tables.inp_reg_form_user_email.send_keys('tester@ttt.tt')
+    page_tables.inp_reg_form_age.send_keys('27')
+    page_tables.inp_reg_form_salary.send_keys('5000')
+    page_tables.inp_reg_form_department.send_keys('QA')
+    page_tables.btn_reg_form_submit.click()
+    time.sleep(2)
+    assert not page_tables.btn_reg_form_submit.exist()
+    assert page_tables.new_row_first_name.get_text() == 'tester'
+    # checking 'Edit' button
+    time.sleep(2)
+    page_tables.btn_edit.click()
+    # checking that 'Edit' shows filled reg form
+    assert page_tables.inp_reg_form_first_name.get_dom_attribute('value') == 'Cierra'
+    # change first name and check if row in table update
+    page_tables.inp_reg_form_first_name.clear()
+    page_tables.inp_reg_form_first_name.send_keys('Francesca')
+    page_tables.btn_reg_form_submit.click()
+    assert page_tables.old_row_first_name.get_text() == 'Francesca'
+    # check 'Delete' button (delete row)
+    page_tables.btn_delete_row.click()
+    assert page_tables.old_row_first_name.get_text() == 'Alden'
 
 def test_tables_navigation(browser):
     # Предусловия
